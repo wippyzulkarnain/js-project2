761a6c8f7d6959bbadb59ce77ac7a995d7b24ba6
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-17 10:28:41 +0700

    Level 0

---
+++ index.js
@@ -0,0 +1,21 @@
+const names = [
+    "Mr. Fahri",
+    "Mr. Abdul",
+    "Mrs. Josephhine",
+    "Mr. Joseph",
+    "Mr. Paul",
+    "Mrs. Paula",
+    "Mr. Hakim"
+  ];
+
+  // Level 0 ------------------------------
+  console.log("Level 0 : Display All Name")
+  names.forEach(
+      function(name){
+          console.log(name)
+      }
+  )
+
+  // Level 1 ------------------------------
+  console.log("")
+  console.log("Level 1: Diplay All Mr.")
